cf898a89bc523f9f47b35f053925212ce5243ce8
Hamed-elhageen <hamedragabelhageen@gmail.com>
2025-11-25 21:47:57 +0200

    finish the project

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -35,7 +35,7 @@ import {
     trigger('scaleIn', [
       transition(':enter', [
         style({ opacity: 0, transform: 'scale(0.5)' }),
-        animate('1s 1s  ease-out', style({ opacity: 1, transform: 'scale(1)' })),
+        animate('1s 2s  ease-out', style({ opacity: 1, transform: 'scale(1)' })),
       ]),
     ]),
     trigger('textReveal', [
