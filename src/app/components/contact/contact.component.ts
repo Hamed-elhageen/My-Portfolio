b6e76ea5bf8fe4d9acfa7eaebc5e4e11302c1ff0
Hamed-elhageen <hamedragabelhageen@gmail.com>
2025-12-05 13:47:15 +0200

    finish updates

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -34,7 +34,7 @@ export class ContactComponent  implements OnInit, AfterViewInit{
       icon: "M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z M15 11a3 3 0 11-6 0 3 3 0 016 0z",
       title: "Location",
       value: "Damanhour, Beheira, Egypt",
-      link: "#",
+      link: "https://maps.app.goo.gl/vvB3QY8fwzZoatqM9",
     },
   ]
 
